7678947696fabb2124d4804a1431cc25a55a6bfc
Noobcoder-xD <157066626+Noobcoder-xD@users.noreply.github.com>
2024-02-11 09:48:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,9 +48,9 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "vkbotsthalpathy")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://pvsmovies:Samuel901@cluster9.gcj9754.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "pvsmovies")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Pvs_Files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
